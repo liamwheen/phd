aea9d43acfcc6386865b835fd1be4cb566abf8ce
Liam Wheen <liamwheen@googlemail.com>
2020-10-19 17:30:08 +0100

    Extract longitude of the perihelion also

--- code/milanko_params.py
+++ code/milanko_params.py
@@ -5,12 +5,12 @@ import csv
 with open('milanko_20P.csv') as f:
     csv_data = csv.reader(f)
     next(csv_data)
-    t, ecc, obliq = [],[],[]
+    t, ecc, obliq, l_peri = [],[],[],[]
     for line in csv_data:
         t.append(int(line[0]))
         ecc.append(float(line[1]))
         obliq.append(float(line[2]))
+        l_peri.append(float(line[3]))
         
-#plt.plot(t[:1000], ecc[:1000])
-#plt.plot(t[:1000], np.array(obliq[:1000])-0.34)
+#plt.plot(t[:28], l_peri[:28])
 #plt.show()
