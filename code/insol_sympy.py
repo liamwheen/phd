2f2bce704344412bbefcb74c54da6c1bf0eceede
Liam Wheen <liamwheen@googlemail.com>
2020-10-20 20:25:44 +0100

    Solve for cos, sin of alpha instead to lighten load

--- code/insol_sympy.py
+++ code/insol_sympy.py
@@ -19,4 +19,6 @@ n = Matrix([cos(theta), sin(theta), 0])
 
 eq = trigsimp(n.dot(circ))
 pprint(eq)
-pprint(solveset(eq, alpha, domain=S.Reals))
+x,y = symbols('x y')
+# Solve for cos,sin of alpha instead to lighten the load (takes ~2 mins)
+pprint(solve([eq.subs(sin(alpha),x).subs(cos(alpha),y), x**2 + y**2-1], [x,y]))
