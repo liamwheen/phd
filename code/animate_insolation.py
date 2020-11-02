03cdf89c3e717efd942d0fc97f2466104c65730a
Liam Wheen <liamwheen@googlemail.com>
2020-11-02 18:51:58 +0000

    Split insolation into simulation and animation files

---
+++ code/animate_insolation.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import insolation
+
+frame_refr = 1
+k2day = 365250
+tmin = 0 # days
+tmax = tmin + 500*k2day
+num_steps = 501
+t_span = np.linspace(tmin,tmax,num_steps)
+
+class Animate:
+    def __init__(self):
+        self.sim = insolation.Insolation()
+        self.insol_vals = np.array([[None]*1]*insolation.num_steps)
+
+    def iter_func(self):
+        for frame, t in enumerate(t_span):
+            self.insol_vals[frame,:] = self.sim.update(t)
+            if frame%frame_refr==0 or t==self.t_span[-1]:
+                yield t
+
+    def update(self, t):
+        self.ax.set_title('Days Since Aphelion: {:.1f}'.format(t%365.25))
+        self.ax.set_ylabel('Semi-minor axis')
+        self.ax.set_xlabel('Semi-major axis')
+        theta = np.linspace(0, 2*np.pi, 100)
+        a,b = self.sim.ellipse_axes(self.sim.eps)
+        sun = np.sqrt(a**2-b**2)
+        self.ellipse.set_xdata(sun+a*np.cos(theta))
+        self.ellipse.set_ydata(b*np.sin(theta))
+        earthx, earthy, _ = self.sim.pol2cart(*self.sim.polar_pos(t))
+        self.earth.set_xdata(earthx)
+        self.earth.set_ydata(earthy)
+        latlon_unit = self.sim.latlon2unit(0,0)
+        lon0x, lon0y, _ = self.sim.rotate_mat(self.sim.beta, self.sim.rho).dot(latlon_unit)
+        self.latlon0.set_xdata([earthx,earthx+1e11*lon0x])
+        self.latlon0.set_ydata([earthy,earthy+1e11*lon0y])
+        self.insol_plot.set_xdata(np.linspace(tmin/365,tmax/365,len(self.insol_vals)))
+        self.insol_plot.set_ydata(self.insol_vals[:,0])
+        #self.insol_plot2.set_xdata(np.linspace(tmin/365,tmax/356,len(self.sim.insol_vals)))
+        #self.insol_plot2.set_ydata(self.sim.insol_vals[:,1])
+        self.insol_ax.set_xlim([tmin/365,max(t/365,tmin/365+1)])
+        self.insol_ax.set_ylim([400,600])
+
+    def init(self):
+        self.ellipse, = self.ax.plot([],[],'m--',linewidth=0.5)
+        self.earth, = self.ax.plot([],[],'co')
+        self.latlon0, = self.ax.plot([],[],'b')
+        self.insol_ax = self.fig.add_subplot(333)
+        self.insol_ax.set_xlabel('Years')
+        self.insol_ax.set_ylabel('Ave Insol')
+        self.insol_ax.yaxis.tick_right()
+        self.insol_plot, = self.insol_ax.plot([],[],'b')
+        #self.insol_plot2, = self.insol_ax.plot([],[],'r')
+        self.ax.plot([0],[0],'yo',linewidth=4)
+        au = insolation.au
+        self.ax.set_xlim([-1.5*au,1.5*au])
+        self.ax.set_ylim([-1.5*au,1.5*au])
+        self.ax.set_aspect('equal')
+        self.insol_ax.set_ylim([0,600])
+        return self.ellipse, self.earth, self.latlon0, self.insol_plot
+
+    def animate(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        ani = FuncAnimation(self.fig, self.update,
+                frames=self.iter_func, init_func=self.init, interval=1, repeat=False)
+        plt.show()
+
+anim = Animate()
+anim.animate()
+ 
--- code/insolation.py
+++ code/insolation.py
@@ -16,7 +16,6 @@ q = 152096508529**2*1.321e3 # scaled irradiance const such that this over R**2 i
 tmin = 0#30*k2day#1015*k2day# how far in the future to start
 tmax = tmin + 500*k2day# days
 num_steps = 501
-frame_refr = 1#num_steps//(1*10**2)
 
 # Interpolate milankovitch data to fit timescale
 # Loads milanko data for future
@@ -30,9 +29,7 @@ class Insolation:
 
     def __init__(self):
         self.t_span = np.linspace(tmin,tmax,num_steps)
-        self.insol_vals = np.array([[None]*2]*num_steps)
         self.milanko_update(tmin)
-        self.pos = self.polar_pos(tmin)
 
     def milanko_update(self, t):
         self.eps = float(eps_func(t/k2day))
@@ -108,62 +105,16 @@ class Insolation:
             if abs((np.pi-self.rho) - (2*np.pi-theta)) < 0.02:
                 return t_summer
 
-    def iter_func(self):
-        for frame, t in enumerate(self.t_span):
+    def update(self, t):
             self.milanko_update(t)
             t = self.last_sum_solst(t)
             self.insol = q/self.polar_pos(t)[0]**2
-            self.pos = self.polar_pos(t)
-            self.insol_vals[frame,:] = [self.I_lat_ave(lat,t) for lat in [65]]
-            if frame%frame_refr==0 or t==self.t_span[-1]:
-                yield t
+            return [self.I_lat_ave(65,t)]
 
-    def update(self, t):
-        self.ax.set_title('Days Since Aphelion: {:.1f}'.format(t%365.25))
-        self.ax.set_ylabel('Semi-minor axis')
-        self.ax.set_xlabel('Semi-major axis')
-        theta = np.linspace(0, 2*np.pi, 100)
-        a,b = self.ellipse_axes(self.eps)
-        sun = np.sqrt(a**2-b**2)
-        self.ellipse.set_xdata(sun+a*np.cos(theta))
-        self.ellipse.set_ydata(b*np.sin(theta))
-        earthx, earthy, _ = self.pol2cart(*self.polar_pos(t))
-        self.earth.set_xdata(earthx)
-        self.earth.set_ydata(earthy)
-        latlon_unit = self.latlon2unit(0,0)
-        lon0x, lon0y, _ = self.rotate_mat(self.beta, self.rho).dot(latlon_unit)
-        self.latlon0.set_xdata([earthx,earthx+1e11*lon0x])
-        self.latlon0.set_ydata([earthy,earthy+1e11*lon0y])
-        self.insol_plot.set_xdata(np.linspace(tmin/365,tmax/365,len(self.insol_vals)))
-        self.insol_plot.set_ydata(self.insol_vals[:,0])
-        #self.insol_plot2.set_xdata(np.linspace(tmin/365,tmax/356,len(self.insol_vals)))
-        #self.insol_plot2.set_ydata(self.insol_vals[:,1])
-        self.insol_ax.set_xlim([tmin/365,max(t/365,tmin/365+1)])
-        self.insol_ax.set_ylim([400,600])
+if __name__ =="__main__":
+    model = Insolation()
+    insol_vals = np.array([None]*num_steps)
+    for i, t in enumerate(model.t_span):
+        insol_vals[i] = model.update(t)[0]
 
-    def init(self):
-        self.ellipse, = self.ax.plot([],[],'m--',linewidth=0.5)
-        self.earth, = self.ax.plot([],[],'co')
-        self.latlon0, = self.ax.plot([],[],'b')
-        self.insol_ax = self.fig.add_subplot(333)
-        self.insol_ax.set_xlabel('Years')
-        self.insol_ax.set_ylabel('Ave Insol')
-        self.insol_ax.yaxis.tick_right()
-        self.insol_plot, = self.insol_ax.plot([],[],'b')
-        #self.insol_plot2, = self.insol_ax.plot([],[],'r')
-        self.ax.plot([0],[0],'yo',linewidth=4)
-        self.ax.set_xlim([-1.5*au,1.5*au])
-        self.ax.set_ylim([-1.5*au,1.5*au])
-        self.ax.set_aspect('equal')
-        self.insol_ax.set_ylim([0,600])
-        return self.ellipse, self.earth, self.latlon0, self.insol_plot
-
-    def animate(self):
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
-        ani = FuncAnimation(self.fig, self.update,
-                frames=self.iter_func, init_func=self.init, interval=1, repeat=False)
-        plt.show()
-
-model = Insolation()
-model.animate()
+    #np.savetxt('insol_vals.csv',insol_vals,delimiter=',')
