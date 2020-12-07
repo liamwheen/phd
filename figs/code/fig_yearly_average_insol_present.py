d3cb958202d109400850f0b26ead75f489127ee6
Liam Wheen <liamwheen@googlemail.com>
2020-12-07 21:17:22 +0000

    Add s(y) graph and table of params

--- figs/code/fig_yearly_average_insol_present.py
+++ figs/code/fig_yearly_average_insol_present.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib import rc
 rc('text', usetex=True)
-rc('font', family='serif',size=17)
+rc('font', family='serif',size=18)
 import sys
 sys.path.append('../../code/')
 sys.path.append('../../code/data')
@@ -12,12 +12,18 @@ from milanko_params import load_milanko
 
 t, ecc, obliq, _ = load_milanko('forward')
 lats = np.linspace(-np.pi/2,np.pi/2,500)
+y = np.sin(lats) 
 insol = calc_yearly_average(obliq[0],lats,ecc[0])
+c_b = (5/16)*(3*np.sin(0.401)**2 - 2)
+budyko_insol = 340*(1 + 0.5*c_b*(3*y**2 - 1))
 fig, ax = plt.subplots()
-ax.plot(lats*180/np.pi,insol,linewidth=3)
+ax.plot(lats*180/np.pi,insol,linewidth=4,label='$Q_{\mathrm{year}}$')
+ax.plot(lats*180/np.pi,budyko_insol,'k--',linewidth=2,label='$Q_\\varepsilon s(y)$')
+ax.legend()
 ax.xaxis.set_major_formatter(FormatStrFormatter('%g$^\circ$'))
 ax.set_xticks([-90,-60,-30,0,30,60,90])
 ax.set_xlabel('Latitude')
 ax.set_ylabel('Average Yearly Insolation (W/m$^2$)')
 plt.tight_layout()
+#plt.show()
 plt.savefig('../yearly_average_insol_present.pdf')
