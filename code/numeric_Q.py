9c11378105294c1f5866c5959ae5c3715b39be44
Liam Wheen <liamwheen@googlemail.com>
2021-02-15 19:50:47 +0000

    Setup to be implemented into budyko, root_scalar for speed

--- code/numeric_Q.py
+++ code/numeric_Q.py
@@ -3,7 +3,7 @@ Calculate Qyear from paper but numerically rather than analytically. This
 allows for albedo function with SZA dependance to be incorporated. It might
 also be used in the sub-year resolution model."""
 import numpy as np
-from scipy.optimize import root
+from scipy.optimize import root_scalar
 from insol_sympy import calc_yearly_average
 from matplotlib import pyplot as plt
 
@@ -19,58 +19,61 @@ year = 365.2425
 beta = 0.4090
 eps = 0.0167
 rho = 2.9101
-phi_n, gamma_n = 100, 100
+phi_n, gamma_n = 50, 25
 phi = np.arcsin(np.linspace(-1,1,phi_n))
 # Longitudinal symmetry means we can save time just looking at one hemisphere
 gamma = np.linspace(0, np.pi,gamma_n) 
 
 rep_phi, rep_gamma = cartesian_product(phi,gamma)
 
-# P and alpha for Asin(x)+Bcos(x) formula
-P = np.sqrt(2*np.sin(beta)*np.sin(rep_phi)*np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi) +
-        np.cos(beta)**2*np.cos(rep_gamma)**2*np.cos(rep_phi)**2 +
-        np.cos(beta)**2*np.cos(rep_phi)**2 - np.cos(beta)**2 -
-        np.cos(rep_gamma)**2*np.cos(rep_phi)**2 + 1) 
-alph = np.arctan2((-np.sin(beta)*np.sin(rep_phi)*np.cos(rho) +
-    np.sin(rep_gamma)*np.sin(rho)*np.cos(rep_phi) -
-    np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi)*np.cos(rho)),(-np.sin(beta)*np.sin(rep_phi)*np.sin(rho)
-        - np.sin(rep_gamma)*np.cos(rep_phi)*np.cos(rho) -
-        np.sin(rho)*np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi)))
-
 def main():
-    num_Q = Q_year(rep_phi,rep_gamma)
+    num_Q = Q_year(beta, rho, eps)
     ana_Q = calc_yearly_average(beta, np.arcsin(np.linspace(-1,1,phi_n)),
             eps)#*(1-0.32) # Average albedo applied over entire Earth
-    #plt.plot(np.linspace(-90,90,phi_n), num_Q)
-    #plt.plot(np.linspace(-90,90,phi_n), ana_Q)
-    #plt.show()
-    print('Mean Error: ',np.mean((abs(num_Q-ana_Q)/ana_Q)))
+    plt.plot(np.linspace(-90,90,phi_n), num_Q)
+    plt.plot(np.linspace(-90,90,phi_n), ana_Q)
+    plt.show()
+    print('Mean Rel Error: ',np.mean((abs(num_Q-ana_Q)/ana_Q)))
 
-def add_albedo(I):
+def sza_albedo(I):
     """ Using fit curve of ocean albedo from
     https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2004GL021180"""
-    # I is the same as cos(sza)
-    #I[I==0]=np.nan
     a,b,c = np.array([0.296875, -0.59375, 0.326875])*2.5 # Account for clouds (mean=0.32 still)
     albedo = a*I**2 + b*I+ c
     return albedo
 
-def I_fast(theta):
-    return P*np.sin(theta+alph)
+def trig_coefs(beta, rho):
+    # mag and phase for Asin(x)+Bcos(x) formula
+    mag = np.sqrt(2*np.sin(beta)*np.sin(rep_phi)*np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi) +
+            np.cos(beta)**2*np.cos(rep_gamma)**2*np.cos(rep_phi)**2 +
+            np.cos(beta)**2*np.cos(rep_phi)**2 - np.cos(beta)**2 -
+            np.cos(rep_gamma)**2*np.cos(rep_phi)**2 + 1) 
+    phase = np.arctan2((-np.sin(beta)*np.sin(rep_phi)*np.cos(rho) +
+        np.sin(rep_gamma)*np.sin(rho)*np.cos(rep_phi) -
+        np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi)*np.cos(rho)),(-np.sin(beta)*np.sin(rep_phi)*np.sin(rho)
+            - np.sin(rep_gamma)*np.cos(rep_phi)*np.cos(rho) -
+            np.sin(rho)*np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi)))
+
+    return mag, phase
+
+def I_fast(mag, phase, theta):
+    return mag*np.sin(theta+phase)
 
 def I_t(t):
     r, theta = polar_pos(eps, np.array([t]))
-    I = I_fast(theta)
+    I = I_fast(mag, phase, theta)
     I[I<0] = 0
+    #I*=1-sza_albedo(I)
     I = I.reshape(phi_n,gamma_n)
     return I
 
-def Q_year(phi, gamma):
+def Q_year(beta, rho, eps):
     t_span = np.linspace(0,year,1000)
+    mag, phase = trig_coefs(beta, rho)
     Is = np.zeros(phi_n*gamma_n)
     rs, thetas = polar_pos(eps, t_span)
     for r, theta in zip(rs,thetas):
-        I = I_fast(theta)
+        I = I_fast(mag, phase, theta)
         I[I<0] = 0
         #I*=1-add_albedo(I)
         Is+=I/r**2
@@ -79,8 +82,8 @@ def Q_year(phi, gamma):
 
 def midpoint_E(M, eps):
     E_func = lambda E: E - eps*np.sin(E) - M
-    mid_E = root(E_func, M)
-    return mid_E.x[0]
+    mid_E = root_scalar(E_func, method='brentq', bracket=(M-eps,M+eps))
+    return mid_E.root
 
 def calc_theta(E, eps):
     sign = np.ones(len(E))# if E < np.pi else -1
@@ -112,7 +115,6 @@ def anim_main():
 
 
 if __name__ == '__main__':
-    #anim_main()
     import cProfile, pstats
     profiler = cProfile.Profile()
     profiler.enable()
