2f726ffd436219c6715b9b5b9bce1d6cbf555c7e
Liam Wheen <liamwheen@googlemail.com>
2021-02-23 11:12:36 +0000

    Update animation func and albedo coefs

--- code/numeric_Q.py
+++ code/numeric_Q.py
@@ -19,8 +19,8 @@ year = 365.2425
 beta = 0.4090
 eps = 0.0167
 rho = 2.9101
-phi_n, gamma_n = 50, 25
-phi = np.arcsin(np.linspace(-1,1,phi_n))
+phi_n, gamma_n = 200, 50
+phi = np.arcsin(np.linspace(0,1,phi_n))
 # Longitudinal symmetry means we can save time just looking at one hemisphere
 gamma = np.linspace(0, np.pi,gamma_n) 
 
@@ -28,17 +28,19 @@ rep_phi, rep_gamma = cartesian_product(phi,gamma)
 
 def main():
     num_Q = Q_year(beta, rho, eps)
-    ana_Q = calc_yearly_average(beta, np.arcsin(np.linspace(-1,1,phi_n)),
-            eps)#*(1-0.32) # Average albedo applied over entire Earth
-    plt.plot(np.linspace(-90,90,phi_n), num_Q)
-    plt.plot(np.linspace(-90,90,phi_n), ana_Q)
+    ana_Q = calc_yearly_average(beta, phi, eps)*(1-0.29)
+    print(np.mean(num_Q))
+    print(np.mean(ana_Q))
+    plt.plot(np.linspace(0,1,phi_n), num_Q)
+    plt.plot(np.linspace(0,1,phi_n), ana_Q)
     plt.show()
-    print('Mean Rel Error: ',np.mean((abs(num_Q-ana_Q)/ana_Q)))
+    #print('Mean Error: ',np.mean((abs(num_Q-ana_Q))))
+    #print('Mean Grad Error: ',np.mean(abs(np.diff(num_Q)-np.diff(ana_Q))/np.diff(ana_Q)))
 
 def sza_albedo(I):
     """ Using fit curve of ocean albedo from
     https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2004GL021180"""
-    a,b,c = np.array([0.296875, -0.59375, 0.326875])*2.5 # Account for clouds (mean=0.32 still)
+    a,b,c = np.array([0.296875, -0.59375, 0.326875])*3.6 # Account for clouds (mean=0.32 still)
     albedo = a*I**2 + b*I+ c
     return albedo
 
@@ -59,14 +61,6 @@ def trig_coefs(beta, rho):
 def I_fast(mag, phase, theta):
     return mag*np.sin(theta+phase)
 
-def I_t(t):
-    r, theta = polar_pos(eps, np.array([t]))
-    I = I_fast(mag, phase, theta)
-    I[I<0] = 0
-    #I*=1-sza_albedo(I)
-    I = I.reshape(phi_n,gamma_n)
-    return I
-
 def Q_year(beta, rho, eps):
     t_span = np.linspace(0,year,1000)
     mag, phase = trig_coefs(beta, rho)
@@ -75,10 +69,15 @@ def Q_year(beta, rho, eps):
     for r, theta in zip(rs,thetas):
         I = I_fast(mag, phase, theta)
         I[I<0] = 0
-        #I*=1-add_albedo(I)
+        I*=1-sza_albedo(I)
         Is+=I/r**2
     Is = Is.reshape(phi_n,gamma_n)/len(t_span)
-    return K/(4*np.pi)*np.mean(Is,1)
+    #Is*=(1-sza_albedo(np.mean(rs)**2*Is))
+    #plt.imshow((1-sza_albedo(np.mean(rs)**2*Is).T))
+    #plt.plot((1-sza_albedo(np.mean(rs)**2*np.mean(Is,1))))
+    #plt.plot(1-sza_albedo(I))
+    #plt.show()
+    return K/(4*np.pi)*np.mean(Is,1)#*(1-sza_albedo(np.mean(rs)**2*np.mean(Is,1)))
 
 def midpoint_E(M, eps):
     E_func = lambda E: E - eps*np.sin(E) - M
@@ -101,13 +100,22 @@ def polar_pos(eps, t):
     r = au*(1 - eps*np.cos(E))
     return r, theta
 
+def I_t(t, mag, phase):
+    r, theta = polar_pos(eps, np.array([t]))
+    I = I_fast(mag, phase, theta)
+    I[I<0] = 0
+    I*=1-sza_albedo(I)
+    I = I.reshape(phi_n,gamma_n)
+    return I
+
 def anim_main():
     import matplotlib.animation as animation
+    mag, phase = trig_coefs(beta, rho)
     def anim(I):
         im.set_data(np.flipud(I))
     def frames():
         for t in np.linspace(0,year,300):
-            yield I_t(t)
+            yield I_t(t, mag, phase)
     fig, ax = plt.subplots()
     im = plt.imshow(np.zeros((len(phi),len(gamma))),vmin=0, vmax=1)
     ani = animation.FuncAnimation(fig,anim,frames=frames,interval=1,repeat=True)
@@ -121,4 +129,4 @@ if __name__ == '__main__':
     main()
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('cumulative')
-    stats.print_stats()
+    #stats.print_stats()
