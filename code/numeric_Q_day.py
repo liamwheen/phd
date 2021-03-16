9e3ca3fef234b34056bb98d67087094470f5dd49
Liam Wheen <liamwheen@googlemail.com>
2021-03-16 20:48:32 +0000

    Separate Q_day and Q_year files

--- code/numeric_Q_day.py
+++ code/numeric_Q_day.py
@@ -24,14 +24,10 @@ beta0 = 0.4090
 eps0 = 0.0167
 rho0 = 2.9101
 eta0 = 0.94
-phi_n, gamma_n = 300, 300
+phi_n, gamma_n = 50, 200
 phi = np.arcsin(np.linspace(0,1,phi_n))
 y = np.linspace(0,1,phi_n)
-#phi = np.linspace(-np.pi/2,np.pi/2,phi_n)
-# Longitudinal symmetry means we can save time just looking at one hemisphere
-# for Q_year, but if using Q_day, we need the full range
 gamma = np.linspace(0, 2*np.pi, gamma_n) 
-year_res = 5000
 
 rep_phi, rep_gamma = cartesian_product(phi,gamma)
 
@@ -40,47 +36,6 @@ def tester():
     print(Q(np.linspace(0,1,300),np.linspace(0,365/2,20)).T.shape)
     plt.plot(Q(np.linspace(0,1,300),np.linspace(0,365/2,20)).T)
     plt.show()
-    """
-    global rep_phi, rep_gamma, gamma_n, phi_n
-    low_res = Q_year(beta0, rho0, eps0)
-    phi_n*=10
-    gamma_n*=50
-    phi = np.arcsin(np.linspace(0,1,phi_n))
-    gamma = np.linspace(0, np.pi, gamma_n)
-    rep_phi, rep_gamma = cartesian_product(phi,gamma)
-    high_res = Q_year(beta0, rho0, eps0)
-    plt.plot(np.linspace(0,1,len(low_res)),low_res)
-    plt.plot(np.linspace(0,1,len(high_res)),high_res)
-    plt.show()
-    """
-
-
-def main():
-    num_Q = Q_year(beta0, rho0, eps0)
-    print(np.mean(num_Q))
-    ice_inds = int((1-eta0)*phi_n)
-    c_b = (5/16)*(3*np.sin(beta0)**2 - 2)
-    Qs = 0.722*(1 + 0.58*c_b*(3*np.sin(phi)**2 -
-        1))*K/(16*np.pi*au**2*np.sqrt(1-eps0**2))
-    Qs[-ice_inds:] = 0.522*(1 + 0.68*c_b*(3*np.sin(phi[-ice_inds:])**2 -
-        1))*K/(16*np.pi*au**2*np.sqrt(1-eps0**2))
-    print(np.mean(Qs))
-    #ana_Q = calc_yearly_average(beta, phi, eps)*(1-0.29)
-    #print(np.mean(num_Q))
-    #print(np.mean(ana_Q))
-    plt.plot(np.linspace(0,1,phi_n), num_Q)
-    plt.plot(np.linspace(0,1,phi_n), Qs)
-    #plt.plot(np.linspace(0,1,phi_n), ana_Q)
-    plt.show()
-    print('Mean Error: ',np.mean((abs(num_Q-Qs)/num_Q)))
-    print('Mean Grad Error: ',np.mean(abs(np.diff(num_Q)-np.diff(Qs))/np.diff(num_Q)))
-
-def sza_albedo(I):
-    albedo = (1+1.21)/(1+1.57*I)*0.25
-    # Ice albedo from y=0.94 onwards
-    ice_inds = int((1-eta)*phi_n*gamma_n)
-    albedo[-ice_inds:] = (1+1.21)/(1+1.57*I[-ice_inds:])*0.42
-    return albedo
 
 def trig_coefs(beta, rho):
     # mag and phase for Asin(x)+Bcos(x) formula
@@ -110,19 +65,6 @@ def Q_days(t, beta, rho, eps):
     I[I<0] = 0
     Q_days = K/(4*np.pi*r**2)*np.mean(I.reshape(-1,phi_n,gamma_n),2).T
     return interp2d(y, t, Q_days.T)
-
-def Q_year(beta, rho, eps):
-    # Returns Qs interpolated function 
-    t_span = np.linspace(0, year, year_res)
-    mag, phase = trig_coefs(beta, rho)
-    Is = np.zeros(phi_n*gamma_n)
-    rs, thetas = polar_pos(eps, t_span)
-    for r, theta in zip(rs,thetas):
-        I = I_fast(mag, phase, theta)
-        I[I<0] = 0
-        Is+=I/r**2
-    Is = Is.reshape(phi_n,gamma_n)/len(t_span)
-    return interp1d(np.linspace(0,1,phi_n),K/(4*np.pi)*np.mean(Is,1))
 
 def midpoint_E(M, eps):
     E_func = lambda E: E - eps*np.sin(E) - M
