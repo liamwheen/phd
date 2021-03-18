515b9cb6b2a2e82f55f126e7913223708a7a513f
Liam Wheen <liamwheen@googlemail.com>
2021-03-18 21:06:01 +0000

    Return to single day Q_day, implement quad and cubic spline

--- code/numeric_Q_day.py
+++ code/numeric_Q_day.py
@@ -1,11 +1,10 @@
 """
-Calculate Qyear from paper but numerically rather than analytically. This
-allows for albedo function with SZA dependance to be incorporated. It might
-also be used in the sub-year resolution model."""
+Calculate Qday from paper but numerically rather than analytically. This
+can be used in the sub-year resolution model."""
 import numpy as np
 from scipy.optimize import root_scalar
-from scipy.integrate import dblquad
-from scipy.interpolate import interp1d, interp2d
+from scipy.integrate import quad_vec
+from scipy.interpolate import interp1d
 from insol_sympy import calc_yearly_average
 from matplotlib import pyplot as plt
 
@@ -24,67 +23,46 @@ beta0 = 0.4090
 eps0 = 0.0167
 rho0 = 2.9101
 eta0 = 0.94
-phi_n, gamma_n = 50, 200
+phi_n, gamma_n = 50, 100
 phi = np.arcsin(np.linspace(0,1,phi_n))
 y = np.linspace(0,1,phi_n)
 gamma = np.linspace(0, 2*np.pi, gamma_n) 
 
 rep_phi, rep_gamma = cartesian_product(phi,gamma)
 
-def tester():
-    Q = Q_days(np.linspace(0,365/2,10),beta0,rho0,eps0)
-    print(Q(np.linspace(0,1,300),np.linspace(0,365/2,20)).T.shape)
-    plt.plot(Q(np.linspace(0,1,300),np.linspace(0,365/2,20)).T)
-    plt.show()
+def I(gamma, phi, beta, rho, theta):
+    return np.maximum((np.sin(gamma)*np.sin(rho - theta) - np.cos(beta)*np.cos(gamma)*np.cos(rho -
+                theta))*np.cos(phi) - np.sin(beta)*np.sin(phi)*np.cos(rho - theta), 0)
 
-def trig_coefs(beta, rho):
-    # mag and phase for Asin(x)+Bcos(x) formula
-    mag = np.sqrt(2*np.sin(beta)*np.sin(rep_phi)*np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi) +
-            np.cos(beta)**2*np.cos(rep_gamma)**2*np.cos(rep_phi)**2 +
-            np.cos(beta)**2*np.cos(rep_phi)**2 - np.cos(beta)**2 -
-            np.cos(rep_gamma)**2*np.cos(rep_phi)**2 + 1) 
-
-    phase = np.arctan2((-np.sin(beta)*np.sin(rep_phi)*np.cos(rho) +
-            np.sin(rep_gamma)*np.sin(rho)*np.cos(rep_phi) -
-            np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi)*np.cos(rho)),
-            (-np.sin(beta)*np.sin(rep_phi)*np.sin(rho) -
-            np.sin(rep_gamma)*np.cos(rep_phi)*np.cos(rho) -
-            np.sin(rho)*np.cos(beta)*np.cos(rep_gamma)*np.cos(rep_phi)))
-
-    return mag, phase
-
-def I_fast(mag, phase, theta):
-    return mag*np.sin(theta.reshape(-1,1)+phase)
-
-def Q_days(t, beta, rho, eps):
-    # Takes array of days of year, 0 is theta=0, 183 is theta=pi
-    # Returns interp2d of shape len(t)*phi_n
+def Q_day(t, beta, rho, eps):
+    # Performs the same as Q_day but integrating over gamma rather than just
+    # taking the mean
     r, theta = polar_pos(eps, t)
-    mag, phase = trig_coefs(beta, rho)
-    I = I_fast(mag, phase, theta)
-    I[I<0] = 0
-    Q_days = K/(4*np.pi*r**2)*np.mean(I.reshape(-1,phi_n,gamma_n),2).T
-    return interp2d(y, t, Q_days.T)
+    I_day,_ = quad_vec(lambda gamma:I(gamma, phi, beta, rho,
+        theta),0,2*np.pi, epsabs=10)
+    Q_day = K/(8*np.pi**2*r**2)*I_day
+    return interp1d(y, Q_day, 'cubic')
 
-def midpoint_E(M, eps):
-    E_func = lambda E: E - eps*np.sin(E) - M
-    mid_E = root_scalar(E_func, method='brentq', bracket=(M-eps,M+eps))
+def E_midpoint(E, M, eps):
+    return E - eps*np.sin(E) - M
+
+def calc_E(M, eps):
+    mid_E = root_scalar(E_midpoint, (M,eps), method='brentq', bracket=(M-eps,M+eps))
     return mid_E.root
 
 def calc_theta(E, eps):
-    sign = np.ones(len(E))
-    sign[(E>np.pi)] = -1
-    return np.pi+sign*2*np.arctan(np.sqrt((1+eps)/(1-eps)*(np.tan(E/2))**2))
+    sign = 1-2*(E>np.pi)
+    return np.pi+2*sign*np.arctan(np.sqrt((1+eps)/(1-eps)*(np.tan(E/2))**2))
 
 def polar_pos(eps, t):
     """ Using Kepler's law:
     https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion#Position_as_a_function_of_time
     """
-    t = (t+year/2)%year
+    t = (t+year/2)%year # Equations assume theta=0 at perihelion 
     M = t*2*np.pi/year
-    E = np.array([midpoint_E(m, eps) for m in M])
-    theta = calc_theta(E, eps)
+    E = calc_E(M, eps)
     r = au*(1 - eps*np.cos(E))
+    theta = calc_theta(E,eps)
     return r, theta
 
 def I_t(t, mag, phase):
@@ -110,7 +88,6 @@ def anim_main():
 
 
 if __name__ == '__main__':
-    tester()
     """
     import cProfile, pstats
     profiler = cProfile.Profile()
