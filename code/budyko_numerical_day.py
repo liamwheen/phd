b18dd41dcfe8be2009a1274987cbe23be24e8ec9
Liam Wheen <liamwheen@googlemail.com>
2021-03-30 16:26:58 +0100

    Add insol line, simplify y_ind

--- code/budyko_numerical_day.py
+++ code/budyko_numerical_day.py
@@ -22,21 +22,22 @@ R = 2*10**7 #J m^-2 K^-1 (Temp damping)
 S = 2*10**9 # degC s (Ice line damping)
 Q_0 = 340.327 #Wm^-2
         
-eta0 = 0.63 # Initial Iceline
-tmin = -60
-tmax = 0 # Years
+eta0 = 0.86 # Initial Iceline
+tmin = -11021
+tmax = tmin+20 # Years
 year_res = 50 # Points per year
 year_span = np.linspace(0,year,year_res)[:-1] # Avoid repeating last point as first point
 
 y_steps = 1000
 t_steps = (tmax-tmin)*year_res
 frame_refr = 1
+tmp = []
         
 def main():
     model = Budyko()
     list(model.iter_func())
-    #plt.plot(model.eta_record)
-    #plt.show()
+    plt.plot(np.array(tmp)-T_ice)
+    plt.show()
     #np.savetxt('budyko_numerical_day_eta.csv',model.eta_record,delimiter=',')
 
 def anim_main():
@@ -44,6 +45,9 @@ def anim_main():
     model.animate()
     model.eta_record[model.eta_record==0] = np.nan
     print(np.nanmean(model.eta_record))
+    #print(max(model.max_T))
+    plt.plot(model.eta_record)
+    plt.show()
 
 class Budyko:
     def __init__(self):
@@ -52,6 +56,7 @@ class Budyko:
         self.t_span = np.linspace(tmin*year2sec,tmax*year2sec,t_steps) #years
         self.delta = self.t_span[1] - self.t_span[0]
         self.eta_record = np.zeros(t_steps//frame_refr) 
+        self.max_T = np.zeros(t_steps//frame_refr)
         milanko_t, milanko_ecc, milanko_obliq, milanko_l_peri = milanko_params.load_milanko('backward')
         krange = range(int(tmin//1000),2+max(1,int(tmax//1000)))
         self.eps_func = interp1d(milanko_t[krange], milanko_ecc[krange])
@@ -67,7 +72,7 @@ class Budyko:
         #round_y = self.y_delta*np.round(y/self.y_delta) 
         #return ((round_y<=round_eta)*alpha_1+(round_y>round_eta)*alpha_2)
         M = 200
-        return 0.47 + 0.15 * (np.tanh(M*(y - self.eta)))
+        return (alpha_1+alpha_2)/2 + (alpha_2-alpha_1)/2 * (np.tanh(M*(y - self.eta)))
 
     def int_T(self, T):
         return np.trapz(self.T,self.y_span)
@@ -77,16 +82,14 @@ class Budyko:
         T, eta = X[:-1],X[-1]
         dT = self.Qs(y)*(1-self.a_eta(y)) - (A + B*T) - C*(T - self.int_T(T))
         T_eta = T[self.y_ind(eta)]
-        deta = T_eta - T_ice
+        deta = (T_eta - T_ice)**1
+        #deta += (deta>0)*deta*2
+        tmp.append(T_eta)
         return np.append(dT/R, deta/S)
 
     def y_ind(self, y):
-        if isinstance(y,float):
-            y_ind = int(y*y_steps)
-            return min(y_steps-1,y_ind)
-        y_ind = (y*y_steps).astype(int)
-        y_ind[y_ind>(y_steps-1)] = y_steps-1
-        return y_ind
+        y_ind = int(y*y_steps)
+        return min(y_steps-1,y_ind)
 
     def milanko_update(self, t):
         eps = float(self.eps_func(t/year2sec/1000))
@@ -113,8 +116,14 @@ class Budyko:
             day_of_year = (frame%year_res)/year_res*year
             self.Qs = Q_day(day_of_year, beta, rho, eps)
             X = self.RK4(np.append(self.T,self.eta), self.dX_dt, self.delta)
-            self.T, self.eta = X[:-1],X[-1]
+            #plt.figure()
+            #plt.plot(self.Qs(self.y_span)*(1-self.a_eta(self.y_span)))
+            #plt.plot((A + B*self.T))
+            #plt.plot(C*(self.T - self.int_T(self.T)))
+            #plt.show()
+            self.T, self.eta = X[:-1],min(X[-1],1)
             self.eta_record[frame//frame_refr] = self.eta
+            self.max_T[frame//frame_refr] = max(self.T)
             if frame%frame_refr==0:
                 yield t
 
@@ -122,14 +131,18 @@ class Budyko:
         self.ax.set_ylabel('$T(y$, {:.1e} years) (°C)'.format(t/year2sec))
         self.temp.set_xdata(self.y_span)
         self.temp.set_ydata(self.T)
+        self.insol.set_xdata(self.y_span)
+        self.insol.set_ydata(self.Qs(self.y_span)/7-40)
         self.ice.set_xdata(self.eta)
         self.ax.set_ylim(-50,50)
         self.ice.set_ydata(self.ax.get_ylim())
 
     def animate(self):
         self.fig, self.ax = plt.subplots()
+        plt.plot([0,1],2*[T_ice])
         self.ydata, self.Tdata = [], []
         self.temp, = self.ax.plot([], [], 'r-', label='Temperature Profile',linewidth=3)
+        self.insol, = self.ax.plot([], [], 'orange', label='Insol',linewidth=2)
         self.ice, = self.ax.plot([], [], 'b-', label='sin(iceline)',linewidth=3)
         self.ax.set_xlim(self.y_span[0], self.y_span[-1])
         self.ax.set_xlabel('y = sin(latitude)')
