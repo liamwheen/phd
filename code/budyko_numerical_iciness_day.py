d01177b74dafe7c973fe9359127422269d69efb3
Liam Wheen <liamwheen@googlemail.com>
2021-04-15 17:48:11 +0100

    Add Q_year cache system, sparse diffusion mat, capitalise consts

--- code/budyko_numerical_iciness_day.py
+++ code/budyko_numerical_iciness_day.py
@@ -9,6 +9,7 @@ from matplotlib.animation import FuncAnimation
 from data import milanko_params # Values given in 1000 year time steps
 from numeric_Q_day import Q_day, phi_n
 import datetime as dt
+from scipy.sparse import csr_matrix
 
 year2sec = 31556952 #Translate time dependent units to 'per year' instead of 'per second'
 year = 365.2425
@@ -24,16 +25,29 @@ R = 1*10**7 #J m^-2 K^-1 (Temp damping)
 S = 1.23*10**9 # degC s (Ice line damping)
 Q_0 = 340.327 #Wm^-2
         
-tmin = -100
-tmax = 0 # Years
-year_res = 5500 # Points per year
-year_span = np.linspace(0,year,year_res+1)[:-1] # Avoid repeating last point as first point
+TMIN = -10
+TMAX = 0 # Years
+YEAR_RES = 3000 # Points per year
+YEAR_SPAN = np.linspace(0,year,YEAR_RES+1)[:-1] # Avoid repeating last point as first point
 
-y_steps = phi_n#50
-t_steps = (tmax-tmin)*year_res
-frame_refr = 200
+Y_STEPS = phi_n
+T_STEPS = (TMAX-TMIN)*YEAR_RES
+FRAME_REFR = 100
 
-zeros_holder = np.zeros(y_steps)
+Y_SPAN = np.linspace(-1,1,Y_STEPS)
+Y_DELTA = Y_SPAN[1] - Y_SPAN[0]
+T_SPAN = np.linspace(TMIN*year2sec,TMAX*year2sec,T_STEPS) #years
+DELTA = T_SPAN[1] - T_SPAN[0]
+
+dd1 = np.zeros((Y_STEPS,Y_STEPS)) + np.diag(np.ones(Y_STEPS-1),1) + np.diag(-np.ones(Y_STEPS-1),-1)
+dd2 = -2*np.eye(Y_STEPS) + np.diag(np.ones(Y_STEPS-1),1) + np.diag(np.ones(Y_STEPS-1),-1)
+#dd1[[0,-1],[1,-2]]=0
+#dd1[[0,-1],[0,-1]]=-1
+#dd2[[0,-1],[1,-2]]=2
+DIFFUSE_MAT = Y_DELTA*np.diag(-Y_SPAN)@dd1 + np.diag(1-Y_SPAN**2)@dd2
+DIFFUSE_MAT*=1/Y_DELTA**2
+DIFFUSE_MAT = csr_matrix(DIFFUSE_MAT)
+
 
 def main():
     model = Budyko()
@@ -51,44 +65,30 @@ def anim_main():
 
 class Budyko:
     def __init__(self):
-        self.y_span = np.linspace(-1,1,y_steps)
-        self.y_delta = self.y_span[1] - self.y_span[0]
-        self.t_span = np.linspace(tmin*year2sec,tmax*year2sec,t_steps) #years
-        self.delta = self.t_span[1] - self.t_span[0]
         milanko_t, milanko_ecc, milanko_obliq, milanko_l_peri = milanko_params.load_milanko('backward')
-        krange = range(int(tmin//1000),2+max(1,int(tmax//1000)))
+        krange = range(int(TMIN//1000),2+max(1,int(TMAX//1000)))
         self.eps_func = interp1d(milanko_t[krange], milanko_ecc[krange])
         self.beta_func = interp1d(milanko_t[krange], milanko_obliq[krange])
         self.l_peri_func = interp1d(milanko_t[krange], milanko_l_peri[krange])
-        self.ice = np.zeros(y_steps)
-        self.T = np.zeros(y_steps)
-        self.ice_record = np.zeros((t_steps//frame_refr,y_steps))
+        self.ice = np.zeros(Y_STEPS)
+        self.T = np.zeros(Y_STEPS)
+        self.ice_record = np.zeros((T_STEPS//FRAME_REFR,Y_STEPS))
 
     def a_ice(self):
         return alpha_1 + self.ice*alpha_2
 
-    def diffuse(self, T, y):
-        dtdx = np.diff(self.T)/self.y_delta
-        y_center = self.y_span[1:]-self.y_delta/2
-        zeros_holder[1:-1] = D*np.diff((1-y_center**2)*dtdx)/self.y_delta
-        return zeros_holder
-    
+    def diffuse(self, T):
+        return DIFFUSE_MAT@T
+
     def dX_dt(self, T, ice):
-        y = self.y_span
-        dT = self.Qs*(1-self.a_ice()) - (A + B*T) + D*self.diffuse(T,y)
-        dT[[0,-1]]=dT[[1,-2]]
+        dT = self.Qs*(1-self.a_ice()) - (A + B*T) + D*self.diffuse(T)
         dice = T_ice - T
         return dT/R, dice/S
 
-    def y_ind(self, y):
-        y = (y+1)/2
-        y_ind = (y*y_steps).astype(int)
-        return np.minimum(y_steps-1,y_ind)
-
     def milanko_update(self, t):
-        eps = float(self.eps_func(t/year2sec/1000))
-        beta = float(self.beta_func(t/year2sec/1000))
-        l_peri = float(self.l_peri_func(t/year2sec/1000))
+        eps = float(self.eps_func(t/1000))
+        beta = float(self.beta_func(t/1000))
+        l_peri = float(self.l_peri_func(t/1000))
         rho = (3/2*np.pi - l_peri)%(2*np.pi)
         return eps, beta, rho
 
@@ -99,28 +99,40 @@ class Budyko:
         k3T, k3ice = ddt(T + h*k2T/2, ice + h*k2ice/2)
         k4T, k4ice = ddt(T+ h*k3T, ice + h*k3ice)
         dT = h/6*(k1T + 2*k2T + 2*k3T + k4T)
+        dT[[0,-1]] = dT[[1,-2]]
         dice = h/6*(k1ice + 2*k2ice + 2*k3ice + k4ice)
         return T+dT, ice+dice
 
     def iter_func(self):
         #Iter over all time points
-        Q_year = np.empty((year_res, y_steps))
-        for frame, t in enumerate(self.t_span):
-            if frame%(t_steps//100)==0:print(frame/t_steps)
-            if frame%(100*year_res) < year_res:
-                if frame%(100*year_res) == 0:
-                    eps, beta, rho = self.milanko_update(t)
-                day_of_year = (frame%year_res)/year_res*year
-                Q_year[frame%year_res,:] = Q_day(day_of_year, beta, rho, eps)
-            self.Qs = Q_year[frame%year_res]
-            self.T, ice_open = self.RK4(self.T, self.ice, self.dX_dt, self.delta)
-            self.ice = np.clip(ice_open,0,1)
-            if frame%frame_refr==0:
-                #day = int((frame%year_res/year_res*year+185)%year)
+        for frame, t in enumerate(T_SPAN):
+            #if frame%(T_STEPS//100)==0:print(frame/T_STEPS)
+            if frame%(100*YEAR_RES) == 0:
+                Q_year = self.get_Q_year(t//year2sec)
+            self.Qs = Q_year[frame%YEAR_RES]
+            dT, dice = self.dX_dt(self.T, self.ice)
+            self.T, ice_open = self.RK4(self.T, self.ice, self.dX_dt, DELTA)
+            ice_open[ice_open<0]=0
+            ice_open[ice_open>1]=1
+            self.ice = ice_open
+            if frame%FRAME_REFR==0:
+                #day = int((frame%YEAR_RES/YEAR_RES*year+185)%year)
                 #input(dt.datetime.strptime(f'{day}', '%j').strftime('%d %B'))
-                self.ice_record[frame//frame_refr,:] = self.ice
-                #self.max_T[frame//frame_refr] = max(self.T)
+                self.ice_record[frame//FRAME_REFR,:] = self.ice
+                #self.max_T[frame//FRAME_REFR] = max(self.T)
                 yield t
+
+    def get_Q_year(self, year):
+        f_name = f'.Q_year_cache/{YEAR_RES}_{Y_STEPS}_{year}'
+        try:
+            with open(f_name,'rb') as f:
+                Q_year = np.fromfile(f).reshape(YEAR_RES,Y_STEPS)
+        except FileNotFoundError:
+            with open(f_name,'wb') as f:
+                eps, beta, rho = self.milanko_update(year)
+                Q_year = Q_day(YEAR_SPAN, beta, rho, eps).T
+                Q_year.tofile(f)
+        return Q_year
 
     def update(self, t):
         self.ax.set_ylabel('$T(y$, {:.1e} years) (°C)'.format(t/year2sec))
@@ -132,11 +144,11 @@ class Budyko:
     def animate(self):
         self.fig, self.ax = plt.subplots()
         plt.plot([-1,1],2*[T_ice],':k',label='T_c')
-        self.temp, = self.ax.plot(self.y_span, self.T, 'r-', label='Temperature',linewidth=3)
-        self.insol, = self.ax.plot(self.y_span, y_steps*[np.nan], 'orange', label='Insol',linewidth=2)
-        self.iciness = self.ax.bar(self.y_span, self.ice, 2.05/y_steps,
+        self.temp, = self.ax.plot(Y_SPAN, self.T, 'r-', label='Temperature',linewidth=3)
+        self.insol, = self.ax.plot(Y_SPAN, Y_STEPS*[np.nan], 'orange', label='Insol',linewidth=2)
+        self.iciness = self.ax.bar(Y_SPAN, self.ice, 2.05/Y_STEPS,
                 label='Iciness (/20)')
-        self.ax.set_xlim(self.y_span[0], self.y_span[-1])
+        self.ax.set_xlim(Y_SPAN[0], Y_SPAN[-1])
         self.ax.set_xlabel('y = sin(latitude)')
         self.fig.legend(framealpha=1)
         self.ax.set_ylim(-50,50)
