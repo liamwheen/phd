0642c31222067368ff79f93fa514a57c83601e45
Liam Wheen <liamwheen@googlemail.com>
2021-04-21 17:18:46 +0100

    Phi now passed to Q_day to support 1 and 2 hemi models

--- code/numeric_Q_day.py
+++ code/numeric_Q_day.py
@@ -23,23 +23,27 @@ beta0 = 0.4090
 eps0 = 0.0167
 rho0 = 2.9101
 eta0 = 0.94
-phi_n, gamma_n = 60, 50
+phi_n, gamma_n = 50, 50
 phi = np.arcsin(np.linspace(-1,1,phi_n))
 #phi = np.linspace(-np.pi/2,np.pi/2,phi_n)
 gamma = np.linspace(0, 2*np.pi, gamma_n) 
 
-rep_phi, rep_gamma = cartesian_product(phi,gamma)
-
 def tester():
-    q = Q_days(0, beta0, rho0, eps0)
-    plt.plot(q)
-    plt.show()
+    q = Q_day(np.linspace(0,year,10000), beta0, rho0, eps0)
+    print(np.mean(q[0,:]), max(q[0,:]))
+    print(np.mean(q[-1,:]),max(q[-1,:]))
+    print(len(np.where(q[0,:]==0)[0]))
+    print(len(np.where(q[-1,:]==0)[0]))
+    print((len(np.where(q[0,:]==0)[0])-len(np.where(q[-1,:]==0)[0]))/len(np.where(q[0,:]==0)[0]))
+    print((max(q[0,:])-max(q[-1,:]))/max(q[0,:]))
+    #plt.plot(q)
+    #plt.show()
 
 def I(gamma, phi, beta, rho, theta):
     return np.maximum((np.sin(gamma)*np.sin(rho - theta) - np.cos(beta)*np.cos(gamma)*np.cos(rho -
                 theta))*np.cos(phi) - np.sin(beta)*np.sin(phi)*np.cos(rho - theta), 0)
 
-def trig_coefs(beta, rho, theta):
+def trig_coefs(beta, rho, theta, phi):
     # mag and phase for Asin(x)+Bcos(x) + c formula
     mag = np.sqrt((-np.sin(beta)**2*np.cos(rho - theta)**2 + 1)*np.cos(phi)**2)
     phase = -np.arctan(np.cos(beta)/np.tan(rho - theta))
@@ -47,16 +51,17 @@ def trig_coefs(beta, rho, theta):
     return mag, phase, c
 
 def I_fast(gamma, mag, phase, c):
-    return np.maximum(mag*np.sin(gamma+phase) + c, 0)
+    I = mag*np.sin(gamma+phase) + c
+    I[I<0] = 0
+    return I
 
-def Q_day(t, beta, rho, eps):
+def Q_day(t, beta, rho, eps, phi=phi):
     # Takes either single or list of days to calculate insol for
     r, theta = polar_pos(eps, t)
-    mag, phase, c = trig_coefs(beta, rho, theta.reshape(-1,1))
+    mag, phase, c = trig_coefs(beta, rho, theta.reshape(-1,1), phi)
     I_day =  quad_vec(lambda gamma: I_fast(gamma, mag, phase, c),
                 0,2*np.pi,epsrel=10)[0]
     Q_day = K/(8*np.pi**2*r**2)*I_day.T
-    #return interp1d(np.sin(phi), Q_day, 'cubic')
     return Q_day
 
 def calc_E(M, eps):
@@ -65,7 +70,6 @@ def calc_E(M, eps):
     return E
 
 def calc_theta(E, eps):
-    #sign = 1-2*(E>np.pi)
     sign = np.ones(np.size(E))
     sign[(E>np.pi)] = -1
     return np.pi+2*sign*np.arctan(np.sqrt((1+eps)/(1-eps)*(np.tan(E/2))**2))
@@ -109,8 +113,8 @@ if __name__ == '__main__':
     import cProfile, pstats
     profiler = cProfile.Profile()
     profiler.enable()
-    main()
+    tester()
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('cumulative')
-    #stats.print_stats()
+    stats.print_stats()
     """
